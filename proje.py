f8c85e19c218e6d26a8277dceaa3cf9600358797
gassama-m <maimounadgassama@gmail.com>
2025-06-20 14:44:52 +0300

    Update proje.py

--- proje.py
+++ proje.py
@@ -1,21 +1,21 @@
 import numpy as numpy
 import pandas as pd
 
-path="C:/Users/Lenovo/Desktop/staj/job_postings.csv"
+path="job_postings.csv"
 df1=pd.read_csv(path)
 column_name="fraudulent"
 #create two csv files; one for false job posting and one for real job postions
 job_authenticity=["real", "false"]
 for i in job_authenticity:
     fraud = df1[df1["fraudulent"] == job_authenticity.index(i)]
-    job_path = "C:/Users/Lenovo/Desktop/staj/"+i+"_postings.csv"
+    job_path = i+"_postings.csv"
     fraud.to_csv(job_path, index=False)
 
     #Create for each fraudulence a excel file with the number of column as sheet, in every sheet we will have 
     #the count of every entry and their pourcentage
-    df2 = pd.read_csv("C:/Users/Lenovo/Desktop/staj/"+i+"_postings.csv")
+    df2 = pd.read_csv(i+"_postings.csv")
 
-    with pd.ExcelWriter(i+"byColumn.xlsx") as writer:
+    with pd.ExcelWriter(i+"_postings_columnbysheet.xlsx") as writer:
          for column in df2.columns:
             nan_count = df2[column].isna().sum()
             nan_percentage = (nan_count / len(df2)) * 100
@@ -40,4 +40,4 @@ for i in job_authenticity:
                 summary = pd.concat([nan_row, value_rows])
 
             summary.to_excel(writer, sheet_name=str(column)[:31])
-print("okkk)")
+print("Done")
